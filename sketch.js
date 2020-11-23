bb501defbd8332da76ebfa91b4053d63a134bd79
tojhe <jinator.biz@gmail.com>
2020-11-23 10:32:51 +0800

    update object base design

--- sketch.js
+++ sketch.js
@@ -10,98 +10,71 @@
 // 2: Model Training: https://editor.p5js.org/codingtrain/sketches/-Ywq20rM9
 // 3: Model Deployment: https://editor.p5js.org/codingtrain/sketches/c5sDNr8eM
 // const count_disp = document.getElementById('count');
-let video;
-let poseNet;
-let pose;
-let skeleton;
-let count = 0;
-let lastPose = "No ex"
+var video;
+var poseNet;
+var pose;
+var skeleton;
 
-let brain;
-let poseLabel = "";
+var count = 0;
+var poseLabel = "";
+var lastPose = "";
+var exercise;
 
-var audio = new Audio('ding.mp3');
+var prevLeftYDist;
+var prevRightYDist;
+var prevLeftXDist;
+var prevRightXDist;
 
-// var up_arr = ["TOP_UP", "MID_UP", "BOT_UP"];
-// var down_arr = ["TOP_DOWN", "MID_DOWN", "BOT_DOWN"];
-// var up_arr = ["UP"];
-// var down_arr = ["DOWN"];
+var audio = new Audio('asset/sound/ding.mp3');
 
 function setup() {
-  createCanvas(640, 480);
-  video = createCapture(VIDEO);
-  video.hide();
-  poseNet = ml5.poseNet(video, modelLoaded);
-  poseNet.on('pose', gotPoses);
+  // Canvas
+  var cnv = createCanvas(640, 480);
+  let x = (windowWidth - width) / 2;
+  let y = (windowHeight - height) / 2;
+  cnv.position(x, y);
 
-  let options = {
-    inputs: 34,
-    outputs: 2,
-    task: 'classification',
-    debug: true
-  }
-  brain = ml5.neuralNetwork(options);
-  const modelInfo = {
-    model: 'models/pushupmodel/model.json',
-    metadata: 'models/pushupmodel/model_meta.json',
-    weights: 'models/pushupmodel/model.weights.bin',
+  // Video capture functionality
+  let videoCaptureSettings = {
+      video: {
+        mandatory: {
+          maxWidth: 640,
+          maxHeight: 480
+      },
+      optional: [{ maxFrameRate: 15 }]
+    },
+    audio: false
   };
-  brain.load(modelInfo, brainLoaded);
-}
+  video = createCapture(videoCaptureSettings);
+  video.hide();
 
-function brainLoaded() {
-  console.log('pose classification ready!');
-  classifyPose();
-}
-
-function classifyPose() {
-  if (pose) {
-    let inputs = [];
-    for (let i = 0; i < pose.keypoints.length; i++) {
-      let x = pose.keypoints[i].position.x;
-      let y = pose.keypoints[i].position.y;
-      inputs.push(x);
-      inputs.push(y);
+  // Load PoseNet Model with ml5 wrapper
+  let poseNetOptions = {
+    architecture: 'ResNet50',
+    outputStride: 32,
+    detectionType: 'single',
+    inputResolution: 161
     }
-    brain.classify(inputs, gotResult);
-  } else {
-    poseLabel = 'Start'
-    setTimeout(classifyPose, 100);
-  }
-}
 
-function gotResult(error, results) {
-  // console.log(results);
-  // console.log(results[0]);
-  // console.log(results[0].label.toUpperCase());
-  // count_disp.innerHTML = count;
-  // console.log(results[0])
-  if (results[0].confidence >= 0.8) {
-    poseLabel = results[0].label.toUpperCase();
-  } 
-  if (lastPose == "DOWN" && poseLabel == "UP") {
-  // if (down_arr.contains(lastPose) && up_arr.contains(poseLabel)) {
-    console.log("lastpose: "+lastPose);
-    console.log("current: " + poseLabel);
-    console.log("current confidence: " + results[0].confidence);
-    audio.play();
-    count++;
-  }
-  // console.log(results[0].confidence);
-  classifyPose();
-  lastPose =  poseLabel
+  poseNet = ml5.poseNet(video, poseNetOptions, modelLoaded);
+  poseNet.on('pose', gotPoses);
+  countRep();
 }
-
 
 function gotPoses(poses) {
+  /*
+  Callback function to handle PoseNet object "pose" event
+  */
   if (poses.length > 0) {
     pose = poses[0].pose;
     skeleton = poses[0].skeleton;
   }
 }
 
-
 function modelLoaded() {
+  /*
+  Callback when poseNet model load is completed
+  */
   console.log('poseNet ready');
 }
 
@@ -115,17 +88,19 @@ function draw() {
     for (let i = 0; i < skeleton.length; i++) {
       let a = skeleton[i][0];
       let b = skeleton[i][1];
-      strokeWeight(2);
-      stroke(0);
+      strokeWeight(1.5);
+      stroke(255, 0, 0);
 
       line(a.position.x, a.position.y, b.position.x, b.position.y);
     }
     for (let i = 0; i < pose.keypoints.length; i++) {
       let x = pose.keypoints[i].position.x;
       let y = pose.keypoints[i].position.y;
-      fill(0);
-      stroke(255);
-      ellipse(x, y, 16, 16);
+      // fill(0);
+      // stroke(255);
+      // ellipse(x, y, 16, 16);
+      fill(255, 0, 0);
+      ellipse(x, y, 10, 10);
     }
   }
   pop();
